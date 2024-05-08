06ba3d010adfdb43c9d9652d356743ea5f0f3f47
Vytautas <krupaviciusvy@gmail.com>
2024-05-08 20:34:56 +0300

    first commit

---
+++ src/main/java/com/binted/Binted/BintedApplication.java
@@ -0,0 +1,22 @@
+package com.binted.Binted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BintedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BintedApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/binted/Binted/BintedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binted.Binted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BintedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
