f76be27e68264b519cf6ea2a125b817b353ca015
Vytautas <krupaviciusvy@gmail.com>
2024-05-21 21:38:54 +0300

    added get method for exercises

--- src/main/java/com/binted/Binted/controller/ExerciseController.java
+++ src/main/java/com/binted/Binted/controller/ExerciseController.java
@@ -2,17 +2,24 @@ package com.binted.Binted.controller;
 
 
 import com.binted.Binted.dto.ExerciseDto;
+import com.binted.Binted.mapper.ExerciseMapper;
+import com.binted.Binted.repository.RecordRepository;
 import com.binted.Binted.service.ExerciseService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.SessionScope;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@SessionScope
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
+    private final RecordRepository recordRepository;
+
 
     @GetMapping("/exercise")
     public ResponseEntity<ExerciseDto> getExerciseAndLogs(@RequestParam(value = "id") Long id) {
@@ -24,6 +31,12 @@ public class ExerciseController {
     public ResponseEntity<ExerciseDto> createExercise(@RequestBody ExerciseDto request) {
         ExerciseDto createdExercise = exerciseService.createExercise(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdExercise);
+    }
+
+    @GetMapping("/exercises")
+    public ResponseEntity<List<ExerciseDto>> getAllExercises() {
+        List<ExerciseDto> exercises = exerciseService.getAllExercises();
+        return ResponseEntity.ok(exercises);
     }
 }
 
